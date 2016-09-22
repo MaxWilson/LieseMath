0ab526afd57c219d5e740e98f77c87c59927deec
Max Wilson <maxw@microsoft.com>
2016-09-22 08:16:02 -0700

    Add number key buttons

--- source/Components.fs
+++ source/Components.fs
@@ -24,16 +24,27 @@ type HelloBox() as this =
         e.preventDefault()
 
     member x.componentDidMount () = ()
-            
+
     member x.render () =        
+        let numKey (n: int) =
+            R.input [
+                    Type "submit"
+                    Value (U2.Case1 (string n))
+                    OnClick (fun _ ->
+                                prob.Advance()
+                                x.setState { data = prob.CurrentProblem })
+                ] []
         R.div [ClassName "commentBox"] [
             // Use ReactHelper.com to build a React Component from a type
             R.h2 [] [unbox x.state.data]
-            R.input [
-                Type "submit"
-                Value (U2.Case1 "OK")
-                OnClick (fun _ -> 
-                            prob.Advance()
-                            x.setState { data = prob.CurrentProblem })
-            ] []
+            numKey 1
+            numKey 2
+            numKey 3
+            numKey 4
+            numKey 5
+            numKey 6
+            numKey 7
+            numKey 8
+            numKey 9
+            numKey 0
         ]
