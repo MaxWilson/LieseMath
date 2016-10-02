a6ff0e30989a81e996e7b9673d93b0a3ffd781d4
Max Wilson <maxw@microsoft.com>
2016-10-02 16:56:14 -0700

    Add options and different bases

--- doPublish.cmd
+++ doPublish.cmd
@@ -1,2 +1,2 @@
 pushd \code\BadanarniMath
-git checkout master && xcopy public\* publish\ /y && webpack -p && git checkout gh-pages && xcopy publish\* /y . && git add . && git commit
+git checkout master && xcopy public\* publish\ /y && fable --target prod && webpack -p && git checkout gh-pages && xcopy publish\* /y . && git add . && git commit
