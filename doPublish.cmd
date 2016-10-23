e74b2d15a87b721bec4060c715481b99c17e521a
Max Wilson <maxw@microsoft.com>
2016-10-22 23:03:13 -0700

    Tweak doPublish file

--- doPublish.cmd
+++ doPublish.cmd
@@ -1,2 +1,2 @@
 pushd \code\BadanarniMath
-git checkout master && xcopy public\* publish\ /y && fable --target prod && webpack -p && git checkout gh-pages && xcopy publish\* /y . && git add . && git commit
+git checkout master && xcopy public\* publish\ /y && fable --target prod && webpack -p && git checkout gh-pages && xcopy publish\* /y . && git add . && git commit -m "New version" --amend && git push -f && git checkout master
