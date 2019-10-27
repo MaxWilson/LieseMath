3292c5b6eb9eb036ea71af6fa99322ea9b5e3c6e
Max Wilson <wilson.max@gmail.com>
2019-10-27 16:51:20 -0700

    Tweak publish script

--- doPublish.cmd
+++ doPublish.cmd
@@ -1,2 +1,3 @@
 pushd ~%dp0
+git worktree add ..\BMathPublish gh-pages
 git checkout master && npm run build && (robocopy /s deploy ..\BMathPublish || echo OK) && pushd ..\BMathPublish && git add . && git commit -m "New Version" && git push && popd
