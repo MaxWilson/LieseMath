da2230322922272b743b03299d1f9da95aba0ea2
Max Wilson <wilson.max@gmail.com>
2019-11-18 11:59:58 -0800

    Correct error in doPublish (missed rename)

--- doPublish.cmd
+++ doPublish.cmd
@@ -1,3 +1,3 @@
 pushd ~%dp0
 git worktree add ..\LieseMathPublish gh-pages
-git checkout master && npm run build && (robocopy /s deploy ..\LieseMathPublish || echo OK) && pushd ..\BMathPublish && git add . && git commit -m "New Version" && git push && popd
+git checkout master && npm run build && (robocopy /s deploy ..\LieseMathPublish || echo OK) && pushd ..\LieseMathPublish && git add . && git commit -m "New Version" && git push && popd
